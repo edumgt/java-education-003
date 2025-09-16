ed9d7509468731c667f542fb6350f495c23da3eb
edumgt <kimdypm@gmail.com>
2025-09-16 17:12:22 +0900

    정리

---
+++ src/main/java/CountCharacters.java
@@ -0,0 +1,24 @@
+package src.main.java;
+//Count characters in a string
+
+import java.util.Scanner;
+
+public class CountCharacters
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a String");
+        String input = scan.nextLine();
+
+        int count =0;
+        for (int i = 0; i < input.length(); i++)
+        {
+            count++;
+        }
+        System.out.print("Total Number of Character:  " + count);
+
+        scan.close();
+    }
+}
---
+++ src/main/java/reverseNoStringBuffer.java
@@ -0,0 +1,13 @@
+package src.main.java;
+public class reverseNoStringBuffer {
+    public static void main(String[] args) {
+        int num = 12345;
+        
+
+        StringBuffer rev;
+        StringBuffer sb = new StringBuffer(String.valueOf(num));
+        StringBuffer revBuffer= sb.reverse();
+        System.out.println("Reverse Number is:--"+ revBuffer);
+    }
+    
+}
